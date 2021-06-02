30431b985175441107db73804b5dfb0854d24504
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-06-02 00:01:38 -0400

    Update bookSlice.js

--- src/slices/bookSlice.js
+++ src/slices/bookSlice.js
@@ -22,8 +22,6 @@ export const { setBookList, setCategoryList } = bookSlice.actions;
 
 export const selectBookList = (state) => state.book.bookList;
 export const selectCategoryList = (state) => state.book.categoryList;
-// export const selectComputerHand = (state) => state.game.computerHand;
-// export const selectScore = (state) => state.game.score;
-// export const selectRuleInfo = (state) => state.game.ruleInfo;
+
 
 export default bookSlice.reducer;
